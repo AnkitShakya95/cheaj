b82122148987930d5a4e286848a9f8f222283c95
AnkitShakya95 <ankitpatni95@gmail.com>
2024-12-16 12:22:46 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,6 +1,10 @@
+import os
+
 # config.py
 BOT_NAME = "Free bot"
 BOT_TOKEN = "6755775439:AAGkahjp3xK71u-jG6V0uQUR-xJgqLPt9yw"
 API_ID = 28328736
 API_HASH = "802254a44896baa87f3083b7af36b2e5"
 MONGO_URI = "mongodb+srv://mrnobody:modernhackers@mrnobody.q8e87ij.mongodb.net/?retryWrites=true&w=majority&appName=MrNobody"
+WEBHOOK = True  # Don't change this
+PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
