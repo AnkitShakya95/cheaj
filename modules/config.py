99a49443b018e75c0106169b440a31bcfc805edf
AnkitShakya95 <ankitpatni95@gmail.com>
2024-12-17 10:51:11 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,10 +1,9 @@
+# Add your details here and then deploy by clicking on HEROKU Deploy button
 import os
 
-# config.py
-BOT_NAME = "URL Uploader"
-BOT_TOKEN = "7535024319:AAFbk6M1WuFQMEhqGsE2B9mg7VzXutJ1UW8"
-API_ID = "22100695"
-API_HASH = "0e8f93300ccbbcd56066e6d790b0d3b2"
-MONGO_URI = "mongodb+srv://ankitpatni95:ankitpatni95@cluster0.jberr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-WEBHOOK = "True"  # Don't change this
-PORT = "8080" # Default to 8000 if not set
+API_ID    = os.environ.get("API_ID", "")
+API_HASH  = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+
+#WEBHOOK = True  # Don't change this
+#PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
