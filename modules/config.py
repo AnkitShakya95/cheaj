f3902ffc12d7ee04018fc9b94f3ef0fcb829a31f
AnkitShakya95 <ankitpatni95@gmail.com>
2024-12-17 10:27:36 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -6,5 +6,5 @@ BOT_TOKEN = "7535024319:AAFbk6M1WuFQMEhqGsE2B9mg7VzXutJ1UW8"
 API_ID = "22100695"
 API_HASH = "0e8f93300ccbbcd56066e6d790b0d3b2"
 MONGO_URI = "mongodb+srv://ankitpatni95:ankitpatni95@cluster0.jberr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-WEBHOOK = True  # Don't change this
-PORT = 8080 # Default to 8000 if not set
+WEBHOOK = "True"  # Don't change this
+PORT = "8080" # Default to 8000 if not set
