9aa7e4afef643bc2eca3509879e7fee95d9014ea
AnkitShakya95 <ankitpatni95@gmail.com>
2024-12-17 10:46:41 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -21,11 +21,7 @@ from pyrogram.errors.exceptions.bad_request_400 import StickerEmojiInvalid
 from pyrogram.types.messages_and_media import message
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from bs4 import BeautifulSoup
-from logs import get_last_two_minutes_logs
 import tempfile
-from db import get_collection, save_name, load_name, save_log_channel_id, load_log_channel_id, save_authorized_users, load_authorized_users, load_allowed_channel_ids, save_allowed_channel_ids, load_accept_logs, save_accept_logs # Import the database functions
-from db import save_bot_running_time, load_bot_running_time, reset_bot_running_time, save_max_running_time, load_max_running_time
-from db import save_queue_file, load_queue_file
 from PIL import Image
 from pytube import Playlist  #Youtube Playlist Extractor
 from yt_dlp import YoutubeDL
@@ -37,14 +33,11 @@ bot = Client("bot",
              api_id=API_ID,
              api_hash=API_HASH)
 
-# Get the MongoDB collection for this bot
-collection = get_collection(BOT_NAME, MONGO_URI)
 # Constants
 OWNER_IDS = [7341059064]  # Replace with the actual owner user IDs
 
 cookies_file_path = "modules/cookies.txt"
 # Global variables
-log_channel_id = -1002383275509
 authorized_users = []
 ALLOWED_CHANNEL_IDS = []
 my_name = "𝐊𝐔𝐍𝐀𝐋❤️"
